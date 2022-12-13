b51a22d9c3cf8a32928436b039ef920e4be8e601
Baihaqi <116696555+G4CENeiz@users.noreply.github.com>
2022-12-13 11:17:21 +0700

    remove warning and add mermaid

--- Daspro/smt 1/bankingCreditCLI/src/main/java/com/baihaqi/bankingcreditcli/BankingCreditCLI.java
+++ Daspro/smt 1/bankingCreditCLI/src/main/java/com/baihaqi/bankingcreditcli/BankingCreditCLI.java
@@ -249,7 +249,6 @@ public class BankingCreditCLI {
         for (int i = 0; i < length; i++) {
             switch (numbers.charAt(i)) {
                 case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' -> {
-                    break;
                 }
                 default -> {
                     return false;
